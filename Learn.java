b26087d84d09f6ef1948000041c89ccfc5ad3e16
Kauhsla0Kumar <kaushalkumar0401@gmail.com>
2023-05-31 12:01:08 +0530

    first commit

---
+++ Learn.java
@@ -0,0 +1,15 @@
+class demo
+{
+   public static void m1()
+   {
+      System.out.println("Hello All");
+   }
+   public static void m2()
+   {
+      System.out.println("Hello All");
+   }
+   public static void m3()
+   {
+      System.out.println("Hello All");
+   }
+}
